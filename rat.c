33f004cfeb0455c27f9b07482beff3bd90ed569c
theoboldalex <theoboldalex@gmail.com>
2025-01-06 21:38:15 +0000

    I mean, it kinda works already if the file is on the current level
    and we are not in the context of a pipe...

--- rat.c
+++ rat.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#define RESET_COLOR "\033[0m"
+#define GREEN       "\033[32m"
+
 bool file_exists(const char *filename) {
   FILE *fp = fopen(filename, "r");
 
@@ -16,16 +19,22 @@ bool file_exists(const char *filename) {
 
 int main(int argc, char *argv[]) {
   char *filepath = argv[1];
+  FILE *file_contents;
+  char line[10];
 
-  printf("Hello, I am rat. You asked me to recite the file: %s\n\n", filepath);
-  
   if (!file_exists(filepath)) {
     printf("The file cannot be read.");
 
     return EXIT_FAILURE;
   }
 
+  printf("%s%s%s\n\n", GREEN, filepath, RESET_COLOR);
+  file_contents = fopen(filepath, "r");
+  while (fgets(line, 10, file_contents) != NULL) {
+    printf("%s", line);
+  }
 
-
+  fclose(file_contents);
+  
   return EXIT_SUCCESS;
 }
