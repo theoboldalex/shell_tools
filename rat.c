9c05efd78dc6a14e6912a5b122ac92a954649b12
theoboldalex <theoboldalex@gmail.com>
2025-01-06 21:05:05 +0000

    initial commit

---
+++ rat.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+void handle_error(char *message) {
+  printf("%s\n", message);
+}
+
+bool file_exists(const char *filename) {
+  FILE *fp = fopen(filename, "r");
+
+  if (fp != NULL) {
+    fclose(fp);
+
+    return true;
+  }
+
+  return false;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc == 0) {
+    // we are calling rat in a pipe
+  }
+
+  if (!file_exists(argv[1])) {
+    handle_error("The file cannot be read.");
+
+    return EXIT_FAILURE;
+  }
+
+  
+
+  return EXIT_SUCCESS;
+}
